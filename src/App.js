e32c4d2640f18f4ab27827ff898ca4f9f5e9ac92
Chintan Mak <chintanmakwana2011@gmail.com>
2022-08-03 02:08:36 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from './Components/Pages/Home';
+import Footer from './Components/Sections/Footer';
 import Navbar from './Components/Sections/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
      <Navbar />
      <Home />
+     <Footer />
     </>
   );
 }
--- src/Components/Sections/Footer.jsx
+++ src/Components/Sections/Footer.jsx
@@ -12,9 +12,20 @@ export default class Footer extends Component {
                 <br />
                 Privacy Policy | Terms of Use | Affiliate Program Disclosure</p>
             </div>
+            <hr />
             <div className='secondside-footer'>
                 <h3>Connect</h3>
-                
+                <p>Facebooklogo</p>
+                <p>Pinterestlogo</p>
+            </div>
+            <hr />
+            <div className='thirdside-footer'>
+                <h3>JOIN THE MURAKAMI MAILING LIST</h3>
+                <div>
+                <input type="test" />
+                <button>submit</button>
+                </div>
+                <p>By clicking submit, I acknowledge that I have read and agree to Penguin Random House's Privacy policy and Terms of use.</p>
             </div>
         </div>
       </>
